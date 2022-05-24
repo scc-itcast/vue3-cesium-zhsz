d38bd5d4a762f0216c0f302392b13ed120baf7b6
scc_itcast <scc15599065860@163.com>
2022-05-24 09:55:26 +0800

    zoom-control扩展

--- es/components/controls/compass/index.mjs
+++ es/components/controls/compass/index.mjs
@@ -1,18 +1,28 @@
-import { defineComponent, getCurrentInstance, ref, reactive, watch, nextTick, computed, h, createCommentVNode } from 'vue';
-import usePosition from '../../../composables/private/use-position.mjs';
-import { getVcParentInstance, $ } from '../../../utils/private/vm.mjs';
-import { defaultProps, defaultOptions } from './defaultProps.mjs';
-import { hMergeSlot } from '../../../utils/private/render.mjs';
-import { VcBtn, VcIcon, VcTooltip } from '../../ui/index.mjs';
-import '../../../composables/index.mjs';
-import useCompass from './use-compass.mjs';
-import { commonEmits } from '../../../utils/emits.mjs';
-import useCommon from '../../../composables/use-common/index.mjs';
-import { useLocale } from '../../../composables/use-locale/index.mjs';
+import {
+  defineComponent,
+  getCurrentInstance,
+  ref,
+  reactive,
+  watch,
+  nextTick,
+  computed,
+  h,
+  createCommentVNode,
+} from "vue";
+import usePosition from "../../../composables/private/use-position.mjs";
+import { getVcParentInstance, $ } from "../../../utils/private/vm.mjs";
+import { defaultProps, defaultOptions } from "./defaultProps.mjs";
+import { hMergeSlot } from "../../../utils/private/render.mjs";
+import { VcBtn, VcIcon, VcTooltip } from "../../ui/index.mjs";
+import "../../../composables/index.mjs";
+import useCompass from "./use-compass.mjs";
+import { commonEmits } from "../../../utils/emits.mjs";
+import useCommon from "../../../composables/use-common/index.mjs";
+import { useLocale } from "../../../composables/use-locale/index.mjs";
 
 const emits = {
   ...commonEmits,
-  compassEvt: (evt) => true
+  compassEvt: (evt) => true,
 };
 const compassProps = defaultProps;
 var Compass = defineComponent({
@@ -34,19 +44,25 @@ var Compass = defineComponent({
     const positionState = usePosition(props, $services);
     const rootRef = ref(null);
     const outerRingRef = ref(null);
-    const hasVcNavigation = ((_a = parentInstance.proxy) == null ? void 0 : _a.$options.name) === "VcNavigation";
+    const hasVcNavigation =
+      ((_a = parentInstance.proxy) == null ? void 0 : _a.$options.name) ===
+      "VcNavigation";
     const canRender = ref(hasVcNavigation);
     const rootStyle = reactive({});
-    watch(() => props, (val) => {
-      nextTick(() => {
-        if (!instance.mounted) {
-          return;
-        }
-        updateRootStyle();
-      });
-    }, {
-      deep: true
-    });
+    watch(
+      () => props,
+      (val) => {
+        nextTick(() => {
+          if (!instance.mounted) {
+            return;
+          }
+          updateRootStyle();
+        });
+      },
+      {
+        deep: true,
+      }
+    );
     const innerOptions = computed(() => {
       return Object.assign({}, defaultOptions.innerOptions, props.innerOptions);
     });
@@ -54,29 +70,37 @@ var Compass = defineComponent({
       return Object.assign({}, defaultOptions.outerOptions, props.outerOptions);
     });
     const markerOptions = computed(() => {
-      return Object.assign({}, defaultOptions.markerOptions, props.markerOptions);
+      return Object.assign(
+        {},
+        defaultOptions.markerOptions,
+        props.markerOptions
+      );
     });
     const outerCircleStyle = computed(() => {
       return {
-        transform: "translate(-50%,-50%) rotate(-" + compassState.heading.value + "rad)",
-        WebkitTransform: "translate(-50%,-50%) rotate(-" + compassState.heading.value + "rad)",
+        transform:
+          "translate(-50%,-50%) rotate(-" + compassState.heading.value + "rad)",
+        WebkitTransform:
+          "translate(-50%,-50%) rotate(-" + compassState.heading.value + "rad)",
         opacity: void 0,
         background: outerOptions.value.background,
-        color: outerOptions.value.color
+        color: outerOptions.value.color,
+        ...props.outerOptions.style,
       };
     });
     const rotationMarkerStyle = computed(() => {
       return {
         transform: "rotate(-" + compassState.orbitCursorAngle.value + "rad)",
-        WebkitTransform: "rotate(-" + compassState.orbitCursorAngle.value + "rad)",
+        WebkitTransform:
+          "rotate(-" + compassState.orbitCursorAngle.value + "rad)",
         opacity: compassState.orbitCursorOpacity.value,
-        color: markerOptions.value.color
+        color: markerOptions.value.color,
       };
     });
     const innerRingStyle = computed(() => {
       const css = {
         background: innerOptions.value.background,
-        color: innerOptions.value.color
+        color: innerOptions.value.color,
       };
       return css;
     });
@@ -99,11 +123,13 @@ var Compass = defineComponent({
       var _a2;
       updateRootStyle();
       const { viewer } = $services;
-      (_a2 = viewer.viewerWidgetResized) == null ? void 0 : _a2.raiseEvent({
-        type: instance.cesiumClass,
-        status: "mounted",
-        target: $(rootRef)
-      });
+      (_a2 = viewer.viewerWidgetResized) == null
+        ? void 0
+        : _a2.raiseEvent({
+            type: instance.cesiumClass,
+            status: "mounted",
+            target: $(rootRef),
+          });
       return compassState.load($services.viewer);
     };
     instance.unmount = async () => {
@@ -111,13 +137,16 @@ var Compass = defineComponent({
       const { viewer } = $services;
       const viewerElement = viewer._element;
       if (!hasVcNavigation) {
-        viewerElement.contains($(rootRef)) && viewerElement.removeChild($(rootRef));
+        viewerElement.contains($(rootRef)) &&
+          viewerElement.removeChild($(rootRef));
       }
-      (_a2 = viewer.viewerWidgetResized) == null ? void 0 : _a2.raiseEvent({
-        type: instance.cesiumClass,
-        status: "unmounted",
-        target: $(rootRef)
-      });
+      (_a2 = viewer.viewerWidgetResized) == null
+        ? void 0
+        : _a2.raiseEvent({
+            type: instance.cesiumClass,
+            status: "unmounted",
+            target: $(rootRef),
+          });
       return compassState.unload();
     };
     const updateRootStyle = () => {
@@ -127,7 +156,8 @@ var Compass = defineComponent({
       rootStyle.top = css.top;
       rootStyle.transform = css.transform;
       const side = positionState.attach.value;
-      const outerRingTarget = (_a2 = $(outerRingRef)) == null ? void 0 : _a2.$el;
+      const outerRingTarget =
+        (_a2 = $(outerRingRef)) == null ? void 0 : _a2.$el;
       if (outerRingTarget !== void 0) {
         const clientRect = outerRingTarget.getBoundingClientRect();
         css.width = `${clientRect.width}px`;
@@ -147,69 +177,118 @@ var Compass = defineComponent({
       if (canRender.value) {
         let children = [];
         children = hMergeSlot(ctx.slots.default, children);
-        children.push(h(VcBtn, {
-          ref: outerRingRef,
-          class: "vc-compass-outerRing absolute-center",
-          style: outerCircleStyle.value,
-          size: outerOptions.value.size,
-          dense: true,
-          round: true,
-          disabled: !props.enableCompassOuterRing
-        }, () => [
-          ctx.slots.outer ? ctx.slots.outer() :
-          h(VcIcon, {
-            size: outerOptions.value.size,
-            name: outerOptions.value.icon
-          }),
-          outerOptions.value.tooltip ? h(VcTooltip, {
-            ref: compassState.iconOuterTooltipRef,
-            ...outerOptions.value.tooltip,
-            onBeforeShow: compassState.onTooltipBeforeShow
-          }, () => h("strong", {}, outerOptions.value.tooltip.tip || t("vc.navigation.compass.outerTip"))) : createCommentVNode("v-if")
-        ]));
-        children.push(h(VcBtn, {
-          class: "vc-compass-innerRing absolute-center",
-          style: innerRingStyle.value,
-          size: innerOptions.value.size,
-          dense: true,
-          round: true
-        }, () => [
-          h(VcIcon, {
-            size: innerOptions.value.size,
-            name: innerOptions.value.icon
-          }),
-          innerOptions.value.tooltip ? h(VcTooltip, {
-            ref: compassState.iconInnerTooltipRef,
-            ...innerOptions.value.tooltip,
-            onBeforeShow: compassState.onTooltipBeforeShow
-          }, () => h("strong", {}, innerOptions.value.tooltip.tip || t("vc.navigation.compass.innerTip"))) : createCommentVNode("v-if")
-        ]));
-        children.push(rotationMarkerStyle.value.opacity ? h(VcBtn, {
-          class: "vc-compass-rotation-marker absolute-center",
-          dense: true,
-          round: true
-        }, () => [
-          h(VcIcon, {
-            size: markerOptions.value.size,
-            name: markerOptions.value.icon,
-            style: rotationMarkerStyle.value
-          })
-        ]) : createCommentVNode("v-if"));
-        return h("div", {
-          ref: rootRef,
-          class: "vc-compass " + positionState.classes.value,
-          style: rootStyle,
-          onDblclick: compassState.handleDoubleClick,
-          onMousedown: compassState.handleMouseDown,
-          onMouseup: compassState.resetRotater,
-          onTouchend: compassState.resetRotater,
-          onTouchstart: compassState.handleMouseDown
-        }, children);
+        children.push(
+          h(
+            VcBtn,
+            {
+              ref: outerRingRef,
+              class: "vc-compass-outerRing absolute-center",
+              style: outerCircleStyle.value,
+              size: outerOptions.value.size,
+              dense: true,
+              round: true,
+              disabled: !props.enableCompassOuterRing,
+            },
+            () => [
+              ctx.slots.outer
+                ? ctx.slots.outer()
+                : h(VcIcon, {
+                    size: outerOptions.value.size,
+                    name: outerOptions.value.icon,
+                  }),
+              outerOptions.value.tooltip
+                ? h(
+                    VcTooltip,
+                    {
+                      ref: compassState.iconOuterTooltipRef,
+                      ...outerOptions.value.tooltip,
+                      onBeforeShow: compassState.onTooltipBeforeShow,
+                    },
+                    () =>
+                      h(
+                        "strong",
+                        {},
+                        outerOptions.value.tooltip.tip ||
+                          t("vc.navigation.compass.outerTip")
+                      )
+                  )
+                : createCommentVNode("v-if"),
+            ]
+          )
+        );
+        children.push(
+          h(
+            VcBtn,
+            {
+              class: "vc-compass-innerRing absolute-center",
+              style: innerRingStyle.value,
+              size: innerOptions.value.size,
+              dense: true,
+              round: true,
+            },
+            () => [
+              h(VcIcon, {
+                size: innerOptions.value.size,
+                name: innerOptions.value.icon,
+              }),
+              innerOptions.value.tooltip
+                ? h(
+                    VcTooltip,
+                    {
+                      ref: compassState.iconInnerTooltipRef,
+                      ...innerOptions.value.tooltip,
+                      onBeforeShow: compassState.onTooltipBeforeShow,
+                    },
+                    () =>
+                      h(
+                        "strong",
+                        {},
+                        innerOptions.value.tooltip.tip ||
+                          t("vc.navigation.compass.innerTip")
+                      )
+                  )
+                : createCommentVNode("v-if"),
+            ]
+          )
+        );
+        children.push(
+          rotationMarkerStyle.value.opacity
+            ? h(
+                VcBtn,
+                {
+                  class: "vc-compass-rotation-marker absolute-center",
+                  dense: true,
+                  round: true,
+                },
+                () => [
+                  h(VcIcon, {
+                    size: markerOptions.value.size,
+                    name: markerOptions.value.icon,
+                    style: rotationMarkerStyle.value,
+                  }),
+                ]
+              )
+            : createCommentVNode("v-if")
+        );
+        return h(
+          "div",
+          {
+            ref: rootRef,
+            class: "vc-compass " + positionState.classes.value,
+            style: rootStyle,
+            onDblclick: compassState.handleDoubleClick,
+            onMousedown: compassState.handleMouseDown,
+            onMouseup: compassState.resetRotater,
+            onTouchend: compassState.resetRotater,
+            onTouchstart: compassState.handleMouseDown,
+          },
+          children
+        );
       } else {
         return createCommentVNode("v-if");
       }
     };
-  }
+  },
 });
 
 export { compassProps, Compass as default };
--- es/components/controls/zoom-control/index.mjs
+++ es/components/controls/zoom-control/index.mjs
@@ -1,20 +1,30 @@
-import { defineComponent, getCurrentInstance, ref, reactive, watch, nextTick, computed, h, createCommentVNode } from 'vue';
-import usePosition from '../../../composables/private/use-position.mjs';
-import { getVcParentInstance, $ } from '../../../utils/private/vm.mjs';
-import { setViewerCamera } from '../../../utils/cesium-helpers.mjs';
-import { hMergeSlot } from '../../../utils/private/render.mjs';
-import { defaultProps, defaultOptions } from './defaultProps.mjs';
-import { VcIcon, VcTooltip, VcBtn } from '../../ui/index.mjs';
-import '../../../composables/index.mjs';
-import useZoomControl from './use-zoom-control.mjs';
-import { kebabCase } from '../../../utils/util.mjs';
-import { commonEmits } from '../../../utils/emits.mjs';
-import useCommon from '../../../composables/use-common/index.mjs';
-import { useLocale } from '../../../composables/use-locale/index.mjs';
+import {
+  defineComponent,
+  getCurrentInstance,
+  ref,
+  reactive,
+  watch,
+  nextTick,
+  computed,
+  h,
+  createCommentVNode,
+} from "vue";
+import usePosition from "../../../composables/private/use-position.mjs";
+import { getVcParentInstance, $ } from "../../../utils/private/vm.mjs";
+import { setViewerCamera } from "../../../utils/cesium-helpers.mjs";
+import { hMergeSlot } from "../../../utils/private/render.mjs";
+import { defaultProps, defaultOptions } from "./defaultProps.mjs";
+import { VcIcon, VcTooltip, VcBtn } from "../../ui/index.mjs";
+import "../../../composables/index.mjs";
+import useZoomControl from "./use-zoom-control.mjs";
+import { kebabCase } from "../../../utils/util.mjs";
+import { commonEmits } from "../../../utils/emits.mjs";
+import useCommon from "../../../composables/use-common/index.mjs";
+import { useLocale } from "../../../composables/use-locale/index.mjs";
 
 const emits = {
   ...commonEmits,
-  zoomEvt: (evt) => true
+  zoomEvt: (evt) => true,
 };
 const zoomControlProps = defaultProps;
 var ZoomControl = defineComponent({
@@ -39,22 +49,34 @@ var ZoomControl = defineComponent({
     const zoomResetRef = ref(null);
     const zoomOutRef = ref(null);
     const parentInstance = getVcParentInstance(instance);
-    const hasVcNavigation = ((_a = parentInstance.proxy) == null ? void 0 : _a.$options.name) === "VcNavigation";
+    const hasVcNavigation =
+      ((_a = parentInstance.proxy) == null ? void 0 : _a.$options.name) ===
+      "VcNavigation";
     const canRender = ref(hasVcNavigation);
     const rootStyle = reactive({});
-    watch(() => props, (val) => {
-      nextTick(() => {
-        if (!instance.mounted) {
-          return;
-        }
-        updateRootStyle();
-      });
-    }, {
-      deep: true
-    });
-    const zoomOutOptions = computed(() => Object.assign({}, defaultOptions.zoomOutOptions, props.zoomOutOptions));
-    const zoomInOptions = computed(() => Object.assign({}, defaultOptions.zoomInOptions, props.zoomInOptions));
-    const zoomResetOptions = computed(() => Object.assign({}, defaultOptions.zoomResetOptions, props.zoomResetOptions));
+    watch(
+      () => props,
+      (val) => {
+        nextTick(() => {
+          if (!instance.mounted) {
+            return;
+          }
+          updateRootStyle();
+        });
+      },
+      {
+        deep: true,
+      }
+    );
+    const zoomOutOptions = computed(() =>
+      Object.assign({}, defaultOptions.zoomOutOptions, props.zoomOutOptions)
+    );
+    const zoomInOptions = computed(() =>
+      Object.assign({}, defaultOptions.zoomInOptions, props.zoomInOptions)
+    );
+    const zoomResetOptions = computed(() =>
+      Object.assign({}, defaultOptions.zoomResetOptions, props.zoomResetOptions)
+    );
     instance.createCesiumObject = async () => {
       return new Promise((resolve, reject) => {
         canRender.value = true;
@@ -78,11 +100,13 @@ var ZoomControl = defineComponent({
       var _a2;
       updateRootStyle();
       const { viewer } = $services;
-      (_a2 = viewer.viewerWidgetResized) == null ? void 0 : _a2.raiseEvent({
-        type: instance.cesiumClass,
-        status: "mounted",
-        target: $(rootRef)
-      });
+      (_a2 = viewer.viewerWidgetResized) == null
+        ? void 0
+        : _a2.raiseEvent({
+            type: instance.cesiumClass,
+            status: "mounted",
+            target: $(rootRef),
+          });
       return true;
     };
     instance.unmount = async () => {
@@ -90,13 +114,16 @@ var ZoomControl = defineComponent({
       const { viewer } = $services;
       if (!hasVcNavigation) {
         const viewerElement = viewer._element;
-        viewerElement.contains($(rootRef)) && viewerElement.removeChild($(rootRef));
+        viewerElement.contains($(rootRef)) &&
+          viewerElement.removeChild($(rootRef));
       }
-      (_a2 = viewer.viewerWidgetResized) == null ? void 0 : _a2.raiseEvent({
-        type: instance.cesiumClass,
-        status: "unmounted",
-        target: $(rootRef)
-      });
+      (_a2 = viewer.viewerWidgetResized) == null
+        ? void 0
+        : _a2.raiseEvent({
+            type: instance.cesiumClass,
+            status: "unmounted",
+            target: $(rootRef),
+          });
       return true;
     };
     const updateRootStyle = () => {
@@ -111,7 +138,8 @@ var ZoomControl = defineComponent({
       css.border = props.border;
       if (!hasVcNavigation) {
         const zoomInTarget = (_a2 = $(zoomInRef)) == null ? void 0 : _a2.$el;
-        const zoomResetTarget = (_b = $(zoomResetRef)) == null ? void 0 : _b.$el;
+        const zoomResetTarget =
+          (_b = $(zoomResetRef)) == null ? void 0 : _b.$el;
         const zoomOutTarget = (_c = $(zoomOutRef)) == null ? void 0 : _c.$el;
         let width = 0;
         let height = 0;
@@ -119,30 +147,44 @@ var ZoomControl = defineComponent({
           const zoomInClientRect = zoomInTarget.getBoundingClientRect();
           if (props.direction === "horizontal") {
             width += zoomInClientRect.width;
-            height = zoomInClientRect.height > height ? zoomInClientRect.height : height;
+            height =
+              zoomInClientRect.height > height
+                ? zoomInClientRect.height
+                : height;
           } else {
             height += zoomInClientRect.height;
-            width = zoomInClientRect.width > width ? zoomInClientRect.width : width;
+            width =
+              zoomInClientRect.width > width ? zoomInClientRect.width : width;
           }
         }
         if (zoomResetTarget !== void 0) {
           const zoomResetClientRect = zoomResetTarget.getBoundingClientRect();
           if (props.direction === "horizontal") {
             width += zoomResetClientRect.width;
-            height = zoomResetClientRect.height > height ? zoomResetClientRect.height : height;
+            height =
+              zoomResetClientRect.height > height
+                ? zoomResetClientRect.height
+                : height;
           } else {
             height += zoomResetClientRect.height;
-            width = zoomResetClientRect.width > width ? zoomResetClientRect.width : width;
+            width =
+              zoomResetClientRect.width > width
+                ? zoomResetClientRect.width
+                : width;
           }
         }
         if (zoomOutTarget !== void 0) {
           const zoomOutClientRect = zoomOutTarget.getBoundingClientRect();
           if (props.direction === "horizontal") {
             width += zoomOutClientRect.width;
-            height = zoomOutClientRect.height > height ? zoomOutClientRect.height : height;
+            height =
+              zoomOutClientRect.height > height
+                ? zoomOutClientRect.height
+                : height;
           } else {
             height += zoomOutClientRect.height;
-            width = zoomOutClientRect.width > width ? zoomOutClientRect.width : width;
+            width =
+              zoomOutClientRect.width > width ? zoomOutClientRect.width : width;
           }
         }
         css.width = `${width + 4}px`;
@@ -157,7 +199,7 @@ var ZoomControl = defineComponent({
           css.transform = "translate(0, -50%)";
         }
       }
-      Object.assign(rootStyle, css);
+      Object.assign(rootStyle, props.style, css);
     };
     const getContent = (options, type) => {
       var _a2, _b, _c;
@@ -168,44 +210,66 @@ var ZoomControl = defineComponent({
       if (type === "zoomIn") {
         btnRef = zoomInRef;
         tooltipRef = zoomControlState.zoomInTooltipRef;
-        tip = ((_a2 = options.tooltip) == null ? void 0 : _a2.tip) || t("vc.navigation.zoomCotrol.zoomInTip");
+        tip =
+          ((_a2 = options.tooltip) == null ? void 0 : _a2.tip) ||
+          t("vc.navigation.zoomCotrol.zoomInTip");
         onClick = zoomControlState.zoomIn;
       } else if (type === "zoomOut") {
         btnRef = zoomOutRef;
         tooltipRef = zoomControlState.zoomOutTooltipRef;
-        tip = ((_b = options.tooltip) == null ? void 0 : _b.tip) || t("vc.navigation.zoomCotrol.zoomOutTip");
+        tip =
+          ((_b = options.tooltip) == null ? void 0 : _b.tip) ||
+          t("vc.navigation.zoomCotrol.zoomOutTip");
         onClick = zoomControlState.zoomOut;
       } else if (type === "zoomReset") {
         btnRef = zoomResetRef;
         tooltipRef = zoomControlState.resetTooltipRef;
-        tip = ((_c = options.tooltip) == null ? void 0 : _c.tip) || t("vc.navigation.zoomCotrol.zoomResetTip");
+        tip =
+          ((_c = options.tooltip) == null ? void 0 : _c.tip) ||
+          t("vc.navigation.zoomCotrol.zoomResetTip");
         onClick = zoomControlState.zoomReset;
       }
       const inner = [];
-      inner.push(h(VcIcon, {
-        name: options.icon,
-        size: options.size
-      }));
+      inner.push(
+        h(VcIcon, {
+          name: options.icon,
+          size: options.size,
+        })
+      );
       inner.push(h("div", null, options.label));
       if (options.tooltip) {
-        inner.push(h(VcTooltip, {
-          ref: tooltipRef,
-          ...options.tooltip
-        }, () => h("strong", null, tip)));
+        inner.push(
+          h(
+            VcTooltip,
+            {
+              ref: tooltipRef,
+              ...options.tooltip,
+            },
+            () => h("strong", null, tip)
+          )
+        );
       } else {
         inner.push(createCommentVNode("v-if"));
       }
-      const content = h(VcBtn, {
-        class: `vc-${kebabCase(type)}`,
-        ref: btnRef,
-        size: options.size,
-        flat: options.flat,
-        stack: options.stack,
-        round: options.round,
-        dense: true,
-        style: { color: options.color, background: options.background },
-        onClick
-      }, () => hMergeSlot(ctx.slots.default, inner));
+      const content = h(
+        VcBtn,
+        {
+          class: `vc-${kebabCase(type)}`,
+          ref: btnRef,
+          size: options.size,
+          flat: options.flat,
+          stack: options.stack,
+          round: options.round,
+          dense: true,
+          style: {
+            color: options.color,
+            background: options.background,
+            ...options.style,
+          },
+          onClick,
+        },
+        () => hMergeSlot(ctx.slots.default, inner)
+      );
       return content;
     };
     return () => {
@@ -213,23 +277,35 @@ var ZoomControl = defineComponent({
         const children = [];
         children.push(h("li", null, getContent(zoomInOptions.value, "zoomIn")));
         if (props.enableResetButton) {
-          children.push(h("li", null, getContent(zoomResetOptions.value, "zoomReset")));
+          children.push(
+            h("li", null, getContent(zoomResetOptions.value, "zoomReset"))
+          );
         } else {
           children.push(createCommentVNode("v-if"));
         }
-        children.push(h("li", null, getContent(zoomOutOptions.value, "zoomOut")));
-        return h("div", {
-          ref: rootRef,
-          class: "vc-zoom-control " + positionState.classes.value,
-          style: rootStyle
-        }, h("ul", {
-          class: "vc-list"
-        }, children));
+        children.push(
+          h("li", null, getContent(zoomOutOptions.value, "zoomOut"))
+        );
+        return h(
+          "div",
+          {
+            ref: rootRef,
+            class: "vc-zoom-control " + positionState.classes.value,
+            style: rootStyle,
+          },
+          h(
+            "ul",
+            {
+              class: "vc-list",
+            },
+            children
+          )
+        );
       } else {
         return createCommentVNode("v-if");
       }
     };
-  }
+  },
 });
 
 export { ZoomControl as default, zoomControlProps };
